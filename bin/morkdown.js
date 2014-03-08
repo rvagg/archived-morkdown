3c0f78d5ccac085379ba1d578f813c3ea998f453
Rod Vagg <rod@vagg.org>
2014-03-07 21:37:37 -0800

    refactored darwin chrome exe lookup

--- bin/morkdown.js
+++ bin/morkdown.js
@@ -22,6 +22,11 @@ var me     = require('..')
   , theme  = argv.theme
 
   , bin    = 'google-chrome'
+  , darwinBin = [
+        '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
+      , '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
+      , '/opt/homebrew-cask/Caskroom/google-chrome/stable-channel/Google Chrome.app/Contents/MacOS/Google Chrome'
+    ]
   , args   = [
         '--app=http://localhost:' + port
       , '--disk-cache-size 0'
@@ -41,19 +46,18 @@ if (!file) {
   process.exit(-1)
 }
 
-me(file, theme, watching).listen(port)
-
 if (os.platform() == 'darwin') {
-  if (fs.existsSync('/Applications/Google Chrome.app/Contents/MacOS/Google Chrome')) {
-    bin = '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome'
-  } else if(fs.existsSync('/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary')) {
-    bin = '/Applications/Google Chrome Canary.app/Contents/MacOS/Google Chrome Canary'
-  } else if (fs.existsSync('/opt/homebrew-cask/Caskroom/google-chrome/stable-channel/Google Chrome.app/Contents/MacOS/Google Chrome')) {
-    bin = '/opt/homebrew-cask/Caskroom/google-chrome/stable-channel/Google Chrome.app/Contents/MacOS/Google Chrome'
-  } else {
-    throw('chrome or canary were not found');
-  }
+  bin = darwinBin.reduce(function (p, c) {
+    if (p)
+      return p
+    return fs.existsSync(c) && c
+  }, null)
+
+  if (!bin)
+    throw(new Error('Chrome or Canary were not found'))
 }
+
+me(file, theme, watching).listen(port)
 
 if (process.env.HOME)
   args.push('--user-data-dir=' + path.join(process.env.HOME, '.md'))
